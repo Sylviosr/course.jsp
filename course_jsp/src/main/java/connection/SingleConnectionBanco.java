87355875e181023edf442f8733edce055797b47a
Ssantarosa <Ssantarosa@DESKTOP-O98S7U8>
2023-09-18 00:03:36 -0300

    Criação do projeto

---
+++ course_jsp/src/main/java/connection/SingleConnectionBanco.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnectionBanco {
+	
+	private static String banco = "jdbc:mysql://localhost:3306/curso_jsp?autoReconnect=true";
+	private static String user = "root";
+	private static String senha = "ester2009";
+	private static Connection connection = null;
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnectionBanco() {/*quando tiver uma instancia vai conectar*/
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+		    
+			if(connection == null) {
+				Class.forName("com.mysql.jdbc.Driver"); /*Carrega o drive de conexão do banco*/
+				connection = DriverManager.getConnection(banco, user, senha);
+				connection.setAutoCommit(false);/*para não efetuar alteraçoes no banco sem nosso comando*/
+			}
+		}catch (Exception e) {
+		 e.printStackTrace();/*Mostrar qualquer erro no momento de conectar*/	
+		}
+	}
+	
+
+}
